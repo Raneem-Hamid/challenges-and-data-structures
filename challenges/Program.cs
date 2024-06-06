dcb6553c5f5352aabb64eb3becc886aef577daaf
Raneem-Hamid <raneemhamid511@gmail.com>
2024-06-06 11:47:04 +0300

    Add project files.

---
+++ challenges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
